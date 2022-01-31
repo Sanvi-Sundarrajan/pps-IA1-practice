32ba7c603d1e25766dd96451370af0fb23796f86
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-01-31 06:06:37 +0000

    p3

--- p3original.c
+++ p3original.c
@@ -9,3 +9,31 @@
 	<br> 1+2+3+4+5 is 15*/
   #include <stdio.h>
   int input_n()
+  {
+    int n;
+    printf("Enter n value \n");
+    scanf("%d",&n);
+    return n;
+  }
+  int sum_n(int n)
+  {
+    int i,sum=0;
+    for(i=0;i<=n;i++)
+    sum=sum+i;
+    return sum;
+  }
+  void output(int n,int sum )
+  {
+    int i;
+    for(i=1;i<n;i++)
+    printf("%d+",i);
+    printf("%d=%d",n,sum);
+  }
+  int main()
+  {
+    int n,sum;
+    n=input_n();
+    sum=sum_n(n);
+    output(n,sum);
+    return 0;
+  }
